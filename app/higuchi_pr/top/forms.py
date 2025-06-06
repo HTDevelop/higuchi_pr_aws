a888df610095ea911a56b6fd751a0517255a1acc
樋口貴志 <higuchitakashi@MacBook-Air.local>
2025-06-06 22:04:17 +0900

    s3に最適化

--- app/higuchi_pr/top/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class SupportMsgForm(forms.Form):
-    msg = forms.CharField(label="メッセージ", max_length=300, widget=forms.Textarea)
--- app/higuchi_pr/top/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-
-class MessageSerializer(serializers.Serializer):
-    msg = serializers.CharField(max_length=300)
